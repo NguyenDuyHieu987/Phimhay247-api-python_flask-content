7122ee943652e3fe5c676aa685ddae4a43a2b2b0
NguyenDuyHieu987 <vaicut6941@gmail.com>
2023-08-21 14:25:23 +0700

    add

--- routes/RecommendRoutes.py
+++ routes/RecommendRoutes.py
@@ -7,7 +7,7 @@ from controllers.RecommendControllers import Recommend
 def recommend_routes(app, cache):
     recomment = Recommend()
 
-    @app.route("/recommend/getrecommend", methods=["GET"])
+    @app.route("/recommend/get", methods=["GET"])
     @cross_origin(origins=configs.ALL_ORIGINS_CONFIG)
     @cache.cached(query_string=True)
     def recommend_route():
