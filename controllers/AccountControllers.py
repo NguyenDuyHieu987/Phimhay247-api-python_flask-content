5749965ce93158f39d8b01bb1e130bda4b53d81e
NguyenDuyHieu987 <104075342+NguyenDuyHieu987@users.noreply.github.com>
2023-12-11 00:43:33 +0700

    add

--- controllers/AccountControllers.py
+++ controllers/AccountControllers.py
@@ -516,10 +516,10 @@ class Account(Database, SendiblueEmail):
             print("start")
             print(request.form)
 
-            print(request.form["new_password"])
-            print(encryptPassword(request.form["new_password"]))
+            print(request.form["new_email"])
+            print(encryptPassword(request.form["new_email"]))
 
-            new_password_encrypted = encryptPassword(request.form["new_password"])
+            new_email_encrypted = encryptPassword(request.form["new_email"])
 
             print("end")
 
