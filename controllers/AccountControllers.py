efaf91208084ae0239e1eda17feba1eb9abaa9e5
NguyenDuyHieu987 <104075342+NguyenDuyHieu987@users.noreply.github.com>
2023-12-10 23:03:20 +0700

    add

--- controllers/AccountControllers.py
+++ controllers/AccountControllers.py
@@ -673,7 +673,7 @@ class Account(Database, SendiblueEmail):
                 algorithms=["HS256"],
             )
 
-            new_password_encrypted = encryptPassword(request.form["new_password"])
+            new_password_encrypted = encryptPassword("123")
 
             account = self.__db["accounts"].find_one_and_update(
                 {
