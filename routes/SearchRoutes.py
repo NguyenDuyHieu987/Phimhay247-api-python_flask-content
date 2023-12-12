d6afeefd4064c424deecf37a40137a96d45c3a11
NguyenDuyHieu987 <104075342+NguyenDuyHieu987@users.noreply.github.com>
2023-12-12 16:41:01 +0700

    add

--- routes/SearchRoutes.py
+++ routes/SearchRoutes.py
@@ -8,24 +8,24 @@ from controllers.SearchControllers import Search
 def search_routes(app, cache):
     search = Search()
 
-    @app.route("/top-search", methods=["GET"])
+    @app.route("/search/top-search", methods=["GET"])
     # @cross_origin(origins=configs.ALL_ORIGINS_CONFIG)
     @cache.cached(query_string=True)
     def top_search_route():
         return search.top_search()
 
-    @app.route("/top-search/search", methods=["GET"])
+    @app.route("/search/top-search/search", methods=["GET"])
     # @cross_origin(origins=configs.ALL_ORIGINS_CONFIG)
     @cache.cached(query_string=True)
     def searchin_top_search_route():
         return search.searchin_top_search()
 
-    @app.route("/search-history", methods=["GET"])
+    @app.route("/search/search-history", methods=["GET"])
     # @cross_origin(origins=configs.ALL_ORIGINS_CONFIG)
     def search_history_route_D():
         return search.search_history()
 
-    @app.route("/search-history/search", methods=["GET"])
+    @app.route("/search/search-history/search", methods=["GET"])
     # @cross_origin(origins=configs.ALL_ORIGINS_CONFIG)
     def searchin_history_route_D():
         return search.searchin_history()
@@ -36,22 +36,22 @@ def search_routes(app, cache):
     def search_route(type):
         return search.search(type)
 
-    @app.route("/add-search", methods=["POST"])
+    @app.route("/search/add-search", methods=["POST"])
     # @cross_origin(origins=configs.ALL_ORIGINS_CONFIG)
     def add_search_route():
         return search.add_search()
 
-    @app.route("/add-history", methods=["POST"])
+    @app.route("/search/add-history", methods=["POST"])
     # @cross_origin(origins=configs.ALL_ORIGINS_CONFIG)
     def add_history_route_D():
         return search.add_history()
 
-    @app.route("/remove-history", methods=["DELETE"])
+    @app.route("/search/remove-history", methods=["DELETE"])
     # @cross_origin(origins=configs.ALL_ORIGINS_CONFIG)
     def remove_history_route_D():
         return search.remove_history()
 
-    @app.route("/clear-history", methods=["DELETE"])
+    @app.route("/search/clear-history", methods=["DELETE"])
     # @cross_origin(origins=configs.ALL_ORIGINS_CONFIG)
     def clear_history_route_D():
         return search.clear_search()
